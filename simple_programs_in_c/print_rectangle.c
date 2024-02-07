54ff033a8b873c809a8af6682cfae2e5950c6e96
berracode <juanguillermoalarcon@gmail.com>
2024-02-07 08:42:40 -0500

    variables in c

---
+++ simple_programs_in_c/print_rectangle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    printf("First form: \n");
+    // first form
+
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+
+    printf("\nSecond form: \n");
+    // second form
+
+    printf("**********\n**********\n**********\n**********\n");
+
+
+
+    return 0;
+}
